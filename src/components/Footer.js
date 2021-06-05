c3b5609ee91cf49913d6509f03c7ac8719a4ae10
danny-1001 <shazam1258@gmail.com>
2021-06-05 19:00:36 -0400

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+function Footer(){
+    return(
+        <footer>
+        <h4>Danny Cortez © 2021 </h4>
+        <ul>
+          <li><a href="#"><img src="/images/youtube-symbol 1.svg" alt="" /></a></li>
+          <li><a href="#"><img src="/images/instagram 1.svg" alt="" /></a></li>
+          <li><a href="#"><img src="/images/twitter 1.svg" alt="" /></a></li>
+        </ul>
+      </footer>
+    );
+}
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import React from 'react'
+function Header(){
+    return(
+        <header className="main-head">
+        <nav>
+          <h1 id="logo"> FotoFox </h1>
+          <ul className="nav-links">
+            <li><a href="#about">About</a></li>
+            <li><a href="#gallery">Gallery</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+          <div className="burger">
+            <span className="line1" />
+            <span className="line2" />
+            <span className="line3" />
+          </div>
+        </nav>
+      </header>
+    );
+}
+export default Header;
